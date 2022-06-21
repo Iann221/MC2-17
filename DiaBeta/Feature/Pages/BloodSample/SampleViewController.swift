6760096284ef5c8da73ad10cfdd88f51a93f3c70
yudxp <yudhahamdi@gmail.com>
2022-06-21 15:31:13 +0700

    Test CoreData

--- DiaBeta/Feature/Pages/BloodSample/SampleViewController.swift
+++ DiaBeta/Feature/Pages/BloodSample/SampleViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class SampleViewController: UIViewController {
 
-    override func viewDidLoad() {
+  @IBOutlet weak var dailyLabel: UILabel!
+  @IBOutlet weak var weeklyLabel: UILabel!
+  @IBOutlet weak var monthlyLabel: UILabel!
+  @IBOutlet weak var higheshContainer: UIView!
+  @IBOutlet weak var lowestContainer: UIView!
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
-
+        dailyLabel.layer.cornerRadius = 20
+        dailyLabel.clipsToBounds = true
+        weeklyLabel.layer.cornerRadius = 20
+        weeklyLabel.clipsToBounds = true
+        monthlyLabel.layer.cornerRadius = 20
+        monthlyLabel.clipsToBounds = true
+        higheshContainer.layer.cornerRadius = 12
+        lowestContainer.layer.cornerRadius = 12
+    
         // Do any additional setup after loading the view.
     }
     
--- DiaBeta/Feature/Pages/Meals/MealsViewController.swift
+++ DiaBeta/Feature/Pages/Meals/MealsViewController.swift
@@ -8,14 +8,66 @@
 import UIKit
 
 class MealsViewController: UIViewController {
-
+  var minuteCount = 0
+  var hourCount = 0
+  var dateDate = "2022/10/1 "
+  var hourHour = 10
+  var minuteMinute = 10
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+  @IBAction func postMealButton(_ sender: Any) {
+    let postValue = Int64.random(in: 111..<160)
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy/MM/dd HH:mm"
+    let sampleData = formatter.date(from: dateDate+String(hourHour)+":"+String(minuteMinute))!
     
+    let heh = UIAlertController(
+      title: "Glucose Edited",
+      message: "This is my first app!",
+      preferredStyle: .alert)
 
+    let haction = UIAlertAction(
+      title: "Awesome",
+      style: .default,
+      handler: nil)
+    
+    DBHelper.shared.editFood(postGula: postValue, timestamp: sampleData)
+    minuteCount += 5
+    hourCount += 2
+    heh.addAction(haction)
+    present(heh, animated: true, completion: nil)
+
+  }
+  
+  @IBAction func insertDataButton(_ sender: Any) {
+    let alert = UIAlertController(
+      title: "Hello, World",
+      message: "This is my first app!",
+      preferredStyle: .alert)
+
+    let action = UIAlertAction(
+      title: "Awesome",
+      style: .default,
+      handler: nil)
+      let formatter = DateFormatter()
+      formatter.dateFormat = "yyyy/MM/dd HH:mm"
+      let sampleData = formatter.date(from: dateDate+String(hourHour)+":"+String(minuteMinute))!
+      let foodName = "Pecel Ayam"
+      let foodCategory = ["Grain", "Protein"]
+      let preValue = Int64.random(in: 90..<110)
+      let image = UIImage(named: "siomay.jpeg")
+      let imageData:NSData = image!.jpegData(compressionQuality: 0.5)! as NSData
+      DBHelper.shared.createFood(timestamp:sampleData, nama: foodName, category: foodCategory, image: imageData, preGula: preValue)
+
+    alert.addAction(action)
+    present(alert, animated: true, completion: nil)
+    
+    
+  }
+  
     /*
     // MARK: - Navigation
 
