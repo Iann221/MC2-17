56621ba801b157bd7523886472d9308dc2450229
Muhammad Abdul Fattah <84857518+fattah247@users.noreply.github.com>
2022-06-23 13:38:46 +0700

    toCoreData
    Kecuali Category (status failed)

---
+++ DiaBeta/Feature/Pages/Food Input/CategoryViewController.swift
@@ -0,0 +1,77 @@
+//
+//  CategoryViewerController.swift
+//  DiaBeta
+//
+//  Created by Muhammad Abdul Fattah on 23/06/22.
+//
+
+import Foundation
+import UIKit
+
+class CategoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  //MARK: - Table Data Source
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return "CATEGORY"
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    category.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    cell.textLabel?.text = category[indexPath.row]
+  return cell
+  }
+  
+  //MARK: - Table Delegate
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    if categoryDict[category[indexPath.row]] == true {
+      categoryDict[category[indexPath.row]] = false
+      tableView.cellForRow(at: indexPath)?.accessoryType = .none
+      
+    } else {
+      categoryDict[category[indexPath.row]] = true
+      tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+    }
+  }
+  
+  //MARK: - Properties
+  @IBOutlet weak var tableView: UITableView! {
+    didSet {
+      tableView.delegate = self
+      tableView.dataSource = self
+      tableView.isScrollEnabled = false
+      
+    }
+  }
+  
+  var category = ["Dairy", "Fruit", "Grains", "Protein", "Starch", "Sweets", "Vegetables"]
+  
+  var categoryDict = ["Dairy": false, "Fruit": false, "Grains": false, "Protein" : false, "Starch" : false, "Sweets" : false, "Vegetables" : false]
+  
+  var categoryIndex = ["Dairy": 1, "Fruit": 2, "Grains": 3, "Protein" : 4, "Starch" : 5, "Sweets" : 6, "Vegetables" : 7]
+  
+  var selectedCategoryString = [String]()
+  var selectedCategoryIndex = [Int]()
+  
+  private func getSelectedCategory(){
+    selectedCategoryString = categoryDict.allKeys(forValue: true)
+    for string in selectedCategoryString {
+      if categoryIndex[string] != nil {
+        selectedCategoryIndex.append(categoryIndex[string]!)
+      }
+    }
+    
+  }
+
+}
---
+++ DiaBeta/Feature/Pages/Food Input/MealsInputViewcontroller.swift
@@ -0,0 +1,218 @@
+import UIKit
+import Photos
+
+class MealsInputViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+ 
+  var imageCoreData: NSData!
+  var namaCoreData: String!
+  //var kategoriCoreData: [String]!
+  var kategoriCoreData = ["Dairy", "Fruit", "Grains", "Protein", "Starch", "Sweets", "Vegetables"]
+  var timeStampCoreData: Date!
+  var preGulaCoreData: Int64!
+  
+  @IBOutlet weak var cameraButton: UIButton!
+  @IBOutlet weak var cameraPreview: UIImageView!
+
+  
+  @IBOutlet weak var DateTime: UIView!
+  @IBOutlet weak var FoodName: UIView!
+  @IBOutlet weak var Category: UIView!
+  @IBOutlet weak var DateView: UIView!
+  @IBOutlet weak var TimeView: UIView!
+  @IBOutlet weak var preGlucoseView: UIView!
+  
+  
+  @IBOutlet weak var preGlucoseTextField: UITextField!
+  @IBOutlet weak var foodTextField: UITextField!
+
+  @IBOutlet weak var CategoryButton: UIButton!
+  @IBOutlet weak var DatePicker: UIDatePicker!
+  @IBOutlet weak var TimePicker: UIDatePicker!
+  
+  @IBOutlet weak var save: UIButton!
+  
+  var imagePickerController = UIImagePickerController()
+  var picker = UIImagePickerController()
+  
+  override func viewDidLoad() {
+  super.viewDidLoad()
+    imagePickerController.delegate = self
+    
+    // BELUM BISA ROUNDED
+//    let image = cameraPreview.image
+//    let imageData:NSData = image!.jpegData(compressionQuality: 0.5)! as NSData
+//    let myImageView:UIImageView = UIImageView()
+//    myImageView.contentMode = UIView.ContentMode.scaleAspectFit
+    cameraPreview.layer.cornerRadius = 8
+    cameraPreview.clipsToBounds = true
+//    myImageView.image = image
+    
+    //Cek Izin
+    checkPermission()
+    
+    
+    
+  }
+  
+//MARK: - Rounding the View
+  private func roundUIView(){
+    DateTime.layer.cornerRadius = 8
+    DateView.layer.cornerRadius = 5
+    TimeView.layer.cornerRadius = 5
+  }
+  
+  
+//MARK: - To Save
+  @IBAction func saveAll(_ sender: Any) {
+    foodTextField.text = namaCoreData
+    preGulaCoreData = Int64(preGlucoseTextField.text!)
+    
+    DBHelper.shared.createFood(timestamp: timeStampCoreData, nama: namaCoreData, category: kategoriCoreData, image: imageCoreData, preGula: preGulaCoreData)
+  }
+  
+  //MARK: - Gabungin Date and Time
+  func dateChanged(sender : UIDatePicker) {
+    let dateFormatr = DateFormatter()
+    dateFormatr.dateFormat = "yyyy-MM-dd"
+    let strDate = dateFormatr.string(from: (DatePicker?.date)!)
+    
+
+ 
+    let dateFormatr2 = DateFormatter()
+    dateFormatr2.dateFormat = "HH:mm:ssZ"
+    let strTime = dateFormatr2.string(from: (TimePicker?.date)!)
+    let strDateTime = strDate+"T"+strTime
+    
+    let dateFormatter = DateFormatter()
+    dateFormatter.locale = Locale(identifier: "en_US_POSIX") // set locale to reliable US_POSIX
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+    timeStampCoreData = dateFormatter.date(from:strDateTime)!
+    
+  }
+  
+  //MARK: - ToCategory
+  
+  @IBAction func tappedCategoryButton(_ sender: Any) {
+    performSegue(withIdentifier: "mealsToCategory", sender: sender)
+  }
+  
+  
+  //MARK: - Image Input
+  
+  @IBAction func tappedCameraButton(_ sender: Any) {
+    let defaultAction = UIAlertAction(title: "Camera",
+                                      style: .default) { [self] (action) in
+   // Respond to user selection of the action.
+              let picker = UIImagePickerController()
+              picker.sourceType = .camera
+              picker.allowsEditing = true
+              picker.delegate = self
+              self.present(picker, animated:true)
+      
+      let imageData:NSData = self.cameraPreview.image!.jpegData(compressionQuality: 0.5)! as NSData
+      imageCoreData = imageData
+      
+    }
+    let cancelAction = UIAlertAction(title: "Gallery",
+                         style: .cancel) { (action) in
+      self.imagePickerController.sourceType = .photoLibrary
+      self.present(self.imagePickerController, animated: true, completion: nil)
+    }
+    
+    // Create and configure the alert controller.
+    let alert = UIAlertController(title: "Method of Input",
+          message: "Please Choose the methode to take the photo",
+          preferredStyle: .alert)
+    alert.addAction(defaultAction)
+    alert.addAction(cancelAction)
+         
+    self.present(alert, animated: true) {
+    }
+  }
+    
+    
+  func checkPermission(){
+    if PHPhotoLibrary.authorizationStatus() != PHAuthorizationStatus.authorized{
+      PHPhotoLibrary.requestAuthorization({(status: PHAuthorizationStatus) -> Void in ()
+      })
+    }
+    if PHPhotoLibrary.authorizationStatus() == PHAuthorizationStatus.authorized{
+    } else{
+      PHPhotoLibrary.requestAuthorization(requestAuthorizationHandler)
+    }
+  }
+  
+  func requestAuthorizationHandler(status: PHAuthorizationStatus){
+    if PHPhotoLibrary.authorizationStatus() == PHAuthorizationStatus.authorized{
+      print("Access granted to use Photo Library")
+    } else{
+      print("We don't have access to your Photos.")
+    }
+  }
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+    if picker.sourceType == .photoLibrary{
+      cameraPreview?.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+    }
+    else{
+      cameraPreview?.image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+    }
+    
+    picker.dismiss(animated: true, completion:nil)
+    
+  }
+  
+  
+  // FOR DATA INPUT
+  
+  // Date
+//  func createDatePicker(){
+//
+//    inputDate.textAlignment = .center
+//    //toolbar
+//    let toolbar = UIToolbar()
+//    toolbar.sizeToFit()
+//
+//    //barbutton
+//    let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+//    toolbar.setItems([doneBtn], animated: true)
+//
+//    //assign toolbar
+//    inputDate.inputAccessoryView = toolbar
+//
+//    //assign date picker to the text field
+//    inputDate.inputView = datePicker
+//
+//    datePicker.datePickerMode = .date
+//
+//  }
+//
+//  @objc func donePressed(){
+//    let formatter = DateFormatter()
+//    formatter.dateStyle = .medium
+//    formatter.timeStyle = .none
+//
+//    inputDate.text = formatter.string(from: datePicker.date)
+//    self.view.endEditing(true)
+//  }
+//
+//
+//  //Time
+//  @objc func timePickerValueChanged(sender: UIDatePicker){
+//    let formatter = DateFormatter()
+//    formatter.locale = Locale(identifier: "en_gb")
+//    formatter.dateFormat = "hh:mm"
+//    inputTime.text = formatter.string(from: sender.date)
+//  }
+  
+  
+  // Private Function
+  
+
+  
+  
+  
+}
+
+
+
