d209db1f6958f039399477e2fbc658c51df8bced
yudxp <yudhahamdi@gmail.com>
2022-06-25 02:24:07 +0700

    Function Good, test merge

--- DiaBeta/Utilities/CoreData/tester/DBTest.swift
+++ DiaBeta/Utilities/CoreData/tester/DBTest.swift
@@ -72,7 +72,7 @@ class DBTest: UIViewController {
         gulaList = DBHelper.shared.getAllGula()
         print(gulaList.count)
         for i in 0..<gulaList.count{
-            print(gulaList[i].event)
+          print(gulaList[i].event!)
         }
 
 //        DBHelper.shared.createInfo(food: foodList[0], type: "bad", selisih: 69)
